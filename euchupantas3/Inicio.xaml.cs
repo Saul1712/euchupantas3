e4906c254c82d625166f66b323689d3e71105a5f
Saul <Saul@DESKTOP-UDNJF2L>
2023-04-18 19:12:26 -0500

    Agregar archivos de proyecto.

---
+++ euchupantas3/Inicio.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace euchupantas3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string datoUno = txtDato.Text;
+            string datoDos = txtDatoDos.Text;
+
+            Navigation.PushAsync( new Registro(datoUno, datoDos));
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ euchupantas3/Registro.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace euchupantas3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string datoUno, string datoDos)
+        {
+            InitializeComponent();
+
+            lblDatoUno.Text = datoUno;
+            txtDatoDos.Text = datoDos;
+        }
+    }
+}
